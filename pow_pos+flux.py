5da428c4773eb19a786b84649573c35ceac5b000
rommuluslewis <rommuluslewis@gmail.com>
2026-04-19 15:09:47 +0800

    changed obs data to be exactly the same as the paper

--- pow_pos+flux.py
+++ pow_pos+flux.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'Lens Models/pow_pos+flux', -1, -1, 1, 1, 0.01, 0.01, 1, verb = 0)
+glafic.init(0.3089, 0.6910, -1.0, 0.6736, 'Lens Models/pow_pos+flux', -2, -1.4, 1.4, 2.0, 0.01, 0.01, 1, verb = 0)
 glafic.set_secondary('chi2_splane 1', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
 glafic.set_secondary('chi2_restart   -1', verb = 0)
@@ -11,12 +11,12 @@ glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(2, 0, 1)
 
-glafic.set_lens(1, 'pow', 0.5, 2.2245, 0.0, 0.0, 0.626, -94.93, 0.446, 2.011)
-glafic.set_lens(2, 'pert', 0.5, 2.2245, 0.0, 0.0, 0.075, -109.26, 0.0, 0.0)
-glafic.set_point(1, 2.2245, 0.0, 0.0)
+glafic.set_lens(1, 'pow', 0.5, 2.2245, -0.3, 0.4, 0.626, -94.93, 0.446, 2.011)
+glafic.set_lens(2, 'pert', 0.5, 2.2245, -0.3, 0.4, 0.075, -109.26, 0.0, 0.0)
+glafic.set_point(1, 2.2245, -0.3, 0.4)
 
-glafic.setopt_lens(1, 1, 0, 1, 1, 1, 1, 1, 1)
-glafic.setopt_lens(2, 1, 0, 0, 0, 1, 1, 0, 0)
+glafic.setopt_lens(1, 0, 0, 1, 1, 0, 0, 0, 0)
+glafic.setopt_lens(2, 0, 0, 1, 1, 0, 0, 0, 0)
 glafic.setopt_point(1, 0, 1, 1)
 
 glafic.model_init(verb = 0)
--- sie_pos.py
+++ sie_pos.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'Lens Models/sie_pos+flux', -1, -1, 1, 1, 0.01, 0.01, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'Lens Models/sie_pos+flux', -1, -0.4, 0.4, 1.0, 0.01, 0.01, 1, verb = 0)
 glafic.set_secondary('chi2_splane 1', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
 glafic.set_secondary('chi2_restart   -1', verb = 0)
@@ -11,8 +11,8 @@ glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(1, 0, 1)
 
-glafic.set_lens(1, 'sie', 0.7, 120, 0.0, 0.0, 0.6, -40, 0.0, 0.0)
-glafic.set_point(1, 2.2245, 0.0, 0.0)
+glafic.set_lens(1, 'sie', 0.5, 120, -0.3, 0.4, 0.6, -40, 0.0, 0.0)
+glafic.set_point(1, 2.2245, -0.3, 0.4)
 
 glafic.setopt_lens(1, 1, 1, 1, 1, 1, 1, 0, 0)
 glafic.setopt_point(1, 0, 1, 1)
